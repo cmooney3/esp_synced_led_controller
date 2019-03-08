24c3fb562f4bcf86d2ffcb1eaf065a377305dd2a
Charlie Mooney <cmooney3@gmail.com>
2019-03-08 10:22:38 -0800

    Hacky -- a better stateless animation to demo how they sync
    This looks better, but the code is super janky -- will update
    later, but for right now it's easily the best sync demo I've
    made
    
    Signed-off-by: Charlie Mooney <cmooney3@gmail.com>

--- animations/scan.h
+++ animations/scan.h
@@ -2,7 +2,7 @@
 #define SCAN_H
 
 #define US_PER_S 1000000
-#define SCAN_RATE_HZ 10
+#define SCAN_RATE_HZ 7
 #define US_PER_TICK (US_PER_S / SCAN_RATE_HZ)
 
 #include "animation.h"
@@ -15,25 +15,25 @@ constexpr int kMaxSpeed = 15;
 class ScanAnimation : public Animation {
   public:
     ScanAnimation(CRGB* leds, int num_leds) : Animation(leds, num_leds) {
-      fillRandomContrastingColors(c1_, c2_);
     }
 
     void generateFrame(uint32_t time) {
       uint32_t ticks = time / US_PER_TICK;
 
-      fill_solid(leds_, num_leds_, c1_);
+      fill_solid(leds_, num_leds_, CRGB(0, 0, 0));
 
-      uint16_t num_positions = (num_leds_ * 2) - 1;
+      uint16_t num_positions = (num_leds_ * 2) - 2;
       uint16_t selected_led_ = ticks % num_positions;
       if (selected_led_ >= num_leds_) {
           selected_led_ = num_positions - selected_led_;
       }
 
-      leds_[selected_led_] = c2_;
+      int i = (ticks / (num_positions * 5)) % 3;
+      leds_[selected_led_] = colors_[i];
     }
 
   private:
-    CRGB c1_, c2_;
+    CRGB colors_[3] = {CRGB(255, 0, 0), CRGB(0, 255, 0), CRGB(0, 0, 255)};
 };
 
 };
