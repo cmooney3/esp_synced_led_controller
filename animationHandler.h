93a3072bc7087d40f3a58ff7170c2e349cb597ab
Charlie Mooney <cmooney3@gmail.com>
2019-03-31 00:32:59 -0700

    Fix the heterogenious animation support
    Signed-off-by: Charlie Mooney <cmooney3@gmail.com>

--- animationHandler.h
+++ animationHandler.h
@@ -6,64 +6,64 @@
 #include "animations/rainbowScan.h"
 #include "animations/sequencedBlink.h"
 
-#define CHIP_ID_NOT_FOUND 0xFFFFFFFF
+#define CHIP_ID_NOT_FOUND 0xFFFF
 
 // These are defined in esp_synced_led_controller
 extern CRGB leds[kNumLEDs];
 extern painlessMesh mesh;
 
-// This struct is filled with the inputs to the animations before each frame is rendered.
-AnimationInputs animation_inputs;
-
 // Here we define the list of animations that the controller can play
 typedef void (*AnimationFunction)(const AnimationInputs&);
 AnimationFunction animations[] = {
-    squencedBlinkAnimation,
     rainbowScanAnimation,
     scanAnimation,
     rainbowFadeAnimation,
+    squencedBlinkAnimation,
 };
 constexpr uint8_t NUM_ANIMATIONS = sizeof(animations) / sizeof(animations[0]);
 
 void fillAnimationInputs(AnimationInputs* inputs) {
     // Basic functionality -- you need the LED structure and the number of LEDs to render any animation
-    animation_inputs.leds = leds;
-    animation_inputs.num_leds = kNumLEDs;
+    inputs->leds = leds;
+    inputs->num_leds = kNumLEDs;
 
     // Simply copy the raw network time (in us) into the struct in case the animation wants it.
-    animation_inputs.raw_time_us = mesh.getNodeTime();
+    inputs->raw_time_us = mesh.getNodeTime();
 
     // Compute the animation number based on the time
-    animation_inputs.animation_number = TO_MS(animation_inputs.raw_time_us) / ANIMATION_DURATION_MS;
+    inputs->animation_number = TO_MS(inputs->raw_time_us) / ANIMATION_DURATION_MS;
 
     // Compute the the time (in us) since the start of this animation
-    int animation_start_time_us = animation_inputs.animation_number * ANIMATION_DURATION_MS * US_PER_MS;
-    animation_inputs.time_since_animation_start_us = animation_inputs.raw_time_us - animation_start_time_us;
+    int animation_start_time_us = inputs->animation_number * ANIMATION_DURATION_MS * US_PER_MS;
+    inputs->time_since_animation_start_us = inputs->raw_time_us - animation_start_time_us;
 
 
     // Get a list of all the chip ids on the network and sort it.  We need this information to determine
     // the controller-specific information that depends on the state of the network below.
     std::list<uint32_t> chip_ids = mesh.getNodeList();
+    chip_ids.push_back(mesh.getNodeId());
 
     // The number of controllers currently in the mesh is just a count of how many chip IDs there are.
-    animation_inputs.num_controllers = chip_ids.size();
+    inputs->num_controllers = chip_ids.size();
 
     // To determine this controllers "controller number" we simply sort the list of chip ids and see where
     // this controller's chip id falls in the list.  This provides a simple way to integrate per-controller
     // effects into your animations.
     chip_ids.sort();
     std::list<uint32_t>::const_iterator it;
-    animation_inputs.this_controller = CHIP_ID_NOT_FOUND;
-    uint32_t i = 0;
+    inputs->this_controller = CHIP_ID_NOT_FOUND;
+    uint16_t i = 0;
+
     for (it = chip_ids.begin(); it != chip_ids.end(); it++) {
         if (*it == mesh.getNodeId()) {
-            animation_inputs.this_controller = i;
+            inputs->this_controller = i;
             break;
         } else {
             i++;
         }
     }
-    if (animation_inputs.this_controller == CHIP_ID_NOT_FOUND) {
+
+    if (inputs->this_controller == CHIP_ID_NOT_FOUND) {
         // This should _never_ happen -- this controller's chip ID should certainly be in the list
         // but just double-checking in case something goes screwy.
         Serial.println("ERROR: This controller's chip ID not found on it's own mesh!");
@@ -71,6 +71,7 @@ void fillAnimationInputs(AnimationInputs* inputs) {
 }
 
 void renderNextFrame() {
+  AnimationInputs animation_inputs;
   fillAnimationInputs(&animation_inputs);
   int selected_animation = (TO_MS(animation_inputs.raw_time_us) / ANIMATION_DURATION_MS) % NUM_ANIMATIONS;
   animations[selected_animation](animation_inputs);
--- animations/sequencedBlink.h
+++ animations/sequencedBlink.h
@@ -3,17 +3,18 @@
 
 #include "animation.h"
 
-constexpr static int kFrameDurationMS = 250;
+constexpr static int kFrameDurationMS = 500;
 
 void squencedBlinkAnimation(const AnimationInputs& inputs) {
-  int frame = frame_number(inputs, kFrameDurationMS);
+    int frame = frame_number(inputs, kFrameDurationMS);
+    uint16_t selected_controller = odometer_signal(frame, 1, inputs.num_controllers);
 
-  int selected_controller = odometer_signal(frame, 1, inputs.num_controllers);
+    CRGB fill_color = CRGB(0, 0, 0);
+    if (selected_controller == inputs.this_controller) {
+        fill_color = random_color(inputs, 0);
+    }
 
-  CRGB fill_color = (selected_controller == inputs.this_controller) ?
-                        random_color(inputs, 0) : CRGB::Black;
-
-  fill_solid(inputs.leds, inputs.num_leds, fill_color);
+    fill_solid(inputs.leds, inputs.num_leds, fill_color);
 }
 
 #endif // SEQUENCED_BLINK_H
